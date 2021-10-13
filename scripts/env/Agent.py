8561a315b9be3baebd331caebb11456d5403800b
Aleksandr <jpc4ar@gmail.com>
2021-10-13 19:40:09 +0300

    workable version of DDP

--- scripts/env/Agent.py
+++ scripts/env/Agent.py
@@ -1,13 +1,14 @@
-import numpy as np
+# import numpy as np
+import autograd.numpy as np
 
 
 class Agent():
     def __init__(self, initial_state=np.array([0., 0., 0., 0., 0.]),  # [x, y, yaw, dv, dyaw]
-                 goal=np.array([10, 10]),
+                 goal=np.array([10, 10, 0, 0, 0]),
                  type="agent",
                  kinematic_type="differencial",
                  dt=0.1,
-                 umax=np.array([5., 3.])):
+                 umax=np.array([0, 0, 0, 5.0, 5.0])):
         self.state_initial = initial_state
         self.goal = goal
         self.type = type
@@ -17,22 +18,42 @@ class Agent():
         self.history = {}
         self.update_history()
     
-    def update_history(self):
-        self.history["state"] = [self.state_initial]
-        self.history["cost"] = [0.]
+    def update_history(self, controll_arr = None):
+        self.history["state"] = [np.copy(self.state_initial)]
+        self.history["controll"] = []
         self.state = np.copy(self.state_initial)
+        if controll_arr is not None:
+            # generate nominal trajectory
+            for u in controll_arr:
+                new_state = self.step(u)
+                self.history["state"].append(np.copy(new_state))
+                self.history["controll"].append(np.copy(u))
 
+    def step_func(self, x, u):
+        if "differencial" in self.kinematic_type:
+            # u = [0,0,0,v,vyaw]
+            pose = x[:3]
+            velocity = x[3:]
+            velocity = self.dt*np.clip(u[3:]+velocity, -self.umax[3:], self.umax[3:])
+            # velocity = self.dt*np.clip(u[3:], -self.umax[3:], self.umax[3:])
+            pose = pose + np.matmul(np.array([[np.cos(x[2]), 0],
+                                              [np.sin(x[2]), 0],
+                                              [0,                    1]]), velocity)
+        return np.concatenate((pose,velocity)) 
+            # x[3:] = self.dt*np.clip(u+x[3:], -self.umax, self.umax)
+            # x[:3] = x[:3] + np.matmul(np.array([[np.cos(x[2]), 0],
+            #                                                       [np.sin(x[2]), 0],
+            #                                                       [0,                    1]]), x[3:])
+        # return x
 
     def step(self, controll):
-        if "differencial" in self.kinematic_type:
-            # u = [v,vyaw]
-            self.state[3:] = np.clip(self.dt*(controll+self.state[3:]), -self.umax, self.umax)
-            self.state[:3] = self.state[:3] + np.matmul(np.array([[np.cos(self.state[2]), 0],
-                                                                  [np.sin(self.state[2]), 0],
-                                                                  [0,                    1]]), self.state[3:])
-            self.history["state"].append(np.copy(self.state))
-        return self.state 
+        self.state = self.step_func(self.state, controll)
+        return self.state
         # action = u[v,yaw]
 
-    def final_cost(self):
-        return np.linalg.norm(self.state[:2]-self.goal)
+    def final_cost(self,state):
+        # state[x,y,yaw]
+        return 10*np.linalg.norm(state[:3]-self.goal[:3])
+
+    def running_cost(self, state, controll):
+        return self.final_cost(state)+0.5*np.sum(np.square(controll))
--- scripts/run.py
+++ scripts/run.py
@@ -1,45 +1,95 @@
 from env.Agent import Agent
 from env.Visualizer_ros import Visualizer_ros
 import numpy as np
-from time import sleep
-import random
+from autograd import grad, jacobian
+import autograd.numpy as np
 
 
-dt = 0.1
-horizon = 20
-epochs = 10
+
+class DDP:
+    def __init__(self, agent) -> None:
+        pred_time = len(agent.history["state"]) - 1
+        self.umax = agent.umax
+        self.dt = agent.dt
+        self.v = [0.0 for _ in range(pred_time + 1)]
+        self.v_x = [np.zeros(state_dim) for _ in range(pred_time + 1)]
+        self.v_xx = [np.zeros((state_dim, state_dim)) for _ in range(pred_time + 1)]
+        self.f = agent.step_func
+        self.lf = agent.final_cost
+        self.lf_x = grad(self.lf)
+        self.lf_xx = jacobian(self.lf_x)
+        self.l_x = grad(agent.running_cost, 0)
+        self.l_u = grad(agent.running_cost, 1)
+        self.l_xx = jacobian(self.l_x, 0)
+        self.l_uu = jacobian(self.l_u, 1)
+        self.l_ux = jacobian(self.l_u, 0)
+        self.f_x = jacobian(self.f, 0)
+        self.f_u = jacobian(self.f, 1)
+        self.f_xx = jacobian(self.f_x, 0)
+        self.f_uu = jacobian(self.f_u, 1)
+        self.f_ux = jacobian(self.f_u, 0)
+
+    def backward(self, x_seq, u_seq):
+        pred_time = len(agent.history["state"]) - 1
+        self.v[-1] = self.lf(x_seq[-1])
+        self.v_x[-1] = self.lf_x(x_seq[-1])
+        self.v_xx[-1] = self.lf_xx(x_seq[-1])
+        k_seq = []
+        kk_seq = []
+        for t in range(pred_time - 1, -1, -1):
+            f_x_t = self.f_x(x_seq[t], u_seq[t])
+            f_u_t = self.f_u(x_seq[t], u_seq[t])
+            q_x = self.l_x(x_seq[t], u_seq[t]) + np.matmul(f_x_t.T, self.v_x[t + 1])
+            q_u = self.l_u(x_seq[t], u_seq[t]) + np.matmul(f_u_t.T, self.v_x[t + 1])
+            q_xx = self.l_xx(x_seq[t], u_seq[t]) + \
+              np.matmul(np.matmul(f_x_t.T, self.v_xx[t + 1]), f_x_t) + \
+              np.dot(self.v_x[t + 1], np.squeeze(self.f_xx(x_seq[t], u_seq[t])))
+            tmp = np.matmul(f_u_t.T, self.v_xx[t + 1])
+            q_uu = self.l_uu(x_seq[t], u_seq[t]) + np.matmul(tmp, f_u_t) + \
+              np.dot(self.v_x[t + 1], np.squeeze(self.f_uu(x_seq[t], u_seq[t])))
+            q_ux = self.l_ux(x_seq[t], u_seq[t]) + np.matmul(tmp, f_x_t) + \
+              np.dot(self.v_x[t + 1], np.squeeze(self.f_ux(x_seq[t], u_seq[t])))
+            inv_q_uu = np.linalg.inv(q_uu)
+            k = -np.matmul(inv_q_uu, q_u)
+            kk = -np.matmul(inv_q_uu, q_ux)
+            dv = 0.5 * np.matmul(q_u, k)
+            self.v[t] += dv
+            self.v_x[t] = q_x - np.matmul(np.matmul(q_u, inv_q_uu), q_ux)
+            self.v_xx[t] = q_xx + np.matmul(q_ux.T, kk)
+            k_seq.append(k)
+            kk_seq.append(kk)
+        k_seq.reverse()
+        kk_seq.reverse()
+        return k_seq, kk_seq
+
+    def forward(self, x_seq, u_seq, k_seq, kk_seq):
+        x_seq_hat = np.array(x_seq)
+        u_seq_hat = np.array(u_seq)
+        for t in range(len(u_seq)):
+            control = self.dt*k_seq[t] + np.matmul(self.dt*kk_seq[t], (x_seq_hat[t] - x_seq[t]))
+            # control = 0.03*k_seq[t] + np.matmul(0.03*kk_seq[t], (x_seq_hat[t] - x_seq[t]))
+            u_seq_hat[t] = np.clip(u_seq[t] + control, -self.umax, self.umax)
+            x_seq_hat[t + 1] = self.f(x_seq_hat[t], u_seq_hat[t])
+        return x_seq_hat, u_seq_hat
 
 if __name__=="__main__":
+    dt = 0.05
+    horizon = 15
+    epochs = 20
+
     viz = Visualizer_ros()
-    agent = Agent()
-    u = np.zeros((horizon,2)) # [[V,Vyaw],[V,Vyaw],...]
-    u[:,0]=1.  
+    agent = Agent(goal=np.array([3, 3, 0, 5., 0.]))
+    u = np.zeros((horizon,agent.state.shape[-1])) # [[V,Vyaw],[V,Vyaw],...]
+    u[:,-2]=2.  
     state_dim = agent.state.shape[-1]
-    # calc nominal trajectory(->agent.history)
-    for controll in u:
-        agent.step(controll)
-        viz.pub_agent_state([agent]) # just vis in rviz
-    agent.update_history()
+    agent.update_history(u) # calc nominal trajectory(->agent.history)
+    viz.pub_agent_state([agent]) # just vis in rviz
+    ddp = DDP(agent) # init differencial functions
     for epoch in range(epochs):
-        # BACKWARD pass
-        V = [0.0 for _ in range(horizon + 1)]
-        V_x = [np.zeros(state_dim) for _ in range(horizon + 1)]
-        V_xx = [np.zeros((state_dim, state_dim)) for _ in range(horizon + 1)]
-        for i in reversed(range(horizon)):
-            # Qx = 
-            # Qu = 
-            # Qxx = 
-            # Quu = 
-            # Qux =
-            # k = -Quu(-1)@Qu
-            # K =  -Quu(-1)@Qux
-            pass
-        # FORWARD pass
-        # calc nominal trajectory(->agent.history)
-        for controll in u:
-            agent.step(controll)
-            viz.pub_agent_state([agent]) # just vis in rviz
-        agent.update_history()
+        k_seq, kk_seq = ddp.backward(agent.history["state"],agent.history["controll"])
+        agent.history["state"], agent.history["controll"] = ddp.forward(agent.history["state"],agent.history["controll"],k_seq, kk_seq)
+        agent.state = np.copy(agent.history["state"][-1])
+        viz.pub_agent_state([agent]) # just vis in rviz
 
 
     exit()
